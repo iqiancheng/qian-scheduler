4529143919992d14178913a09baacb6a06365ec1
Joseph千橙 <mgr.qiancheng@gmail.com>
2016-06-14 07:59:48 +0800

    add jgit depandency

--- src/main/java/com/challions/schedule/controller/TaskController.java
+++ src/main/java/com/challions/schedule/controller/TaskController.java
@@ -1,10 +1,13 @@
 package com.challions.schedule.controller;
 
+import com.challions.schedule.model.TaskEntity;
+import com.challions.schedule.service.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 
@@ -15,6 +18,9 @@ public class TaskController {
 	@Autowired
 	private ThreadPoolTaskScheduler taskScheduler;
 
+    @Autowired
+    private ScheduleService scheduleService;
+
     /**
      * create new cron task
      * 
@@ -23,8 +29,10 @@ public class TaskController {
     @RequestMapping
     public String list() {
         ScheduledExecutorService scheduler = taskScheduler.getScheduledExecutor();
-        ScheduledThreadPoolExecutor executor =  taskScheduler.getScheduledThreadPoolExecutor();
-        executor.getQueue();
+        ScheduledThreadPoolExecutor executor =  taskScheduler..getScheduledThreadPoolExecutor();
+        BlockingQueue<Runnable> queue = executor.getQueue();
+        scheduleService.;
+
         return "tasks";
     }
 
